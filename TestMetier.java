ad50b93c24855f3762c8c9de5b3b0c9e3617939e
Dakhlawie <115040513+Dakhlawie@users.noreply.github.com>
2023-02-25 19:07:15 +0100

    Add files via upload

---
+++ TestMetier.java
@@ -0,0 +1,21 @@
+package Test;
+
+import java.util.List;
+
+import metier.MetierImpl;
+import metier.Produit;
+
+public class TestMetier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MetierImpl metier= new MetierImpl();
+		List<Produit> prods = metier.getProduitsParMotCle("7+");
+		for (Produit p : prods)
+		System.out.println(p.getNomProduit());
+		}
+
+
+	}
+
+
